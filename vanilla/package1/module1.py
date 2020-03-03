54f505fad39327240942a32cd658132c8ec7a701
Fanchouille <francois.valadier@openvalue.fr>
2020-03-03 16:02:35 +0100

    Type methods in class1

--- vanilla/package1/module1.py
+++ vanilla/package1/module1.py
@@ -2,12 +2,12 @@ class Class1:
     def __init__(self, is_reversed: bool):
         self.is_reversed = is_reversed
 
-    def say_hello(self, name):
+    def say_hello(self, name: str) -> None:
         if self.is_reversed:
             print("Hello {} !".format(self.__reverse_name(name)))
         else:
             print("Hello {} !".format(name))
 
-    def __reverse_name(self, name):
+    def __reverse_name(self, name: str) -> str:
         # __ prefix means it is a private method
         return name[::-1]
